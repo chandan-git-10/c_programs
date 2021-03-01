125731c329cf233da1999f5febf36df20c1ec1ef
chandan <chandan@chandan.git.10@gmail.com>
2021-03-01 11:18:01 +0530

    created file_fputc.c

---
+++ c_programs/file_fputc.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+void main()
+{
+	int ch;
+	FILE *fp;
+	if((fp=fopen("myfile","w"))==NULL)
+	{
+		printf("Erro in opening file");
+		exit(1);
+	}
+	printf("enter the text:");
+	while((ch=getchar())!=EOF)
+		fputc(ch,fp);
+	fclose(fp);
+}
