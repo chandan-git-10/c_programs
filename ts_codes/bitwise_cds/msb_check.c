ac01b16a826d3849a7dce79c3b0da1f96b7253d6
chandan <chandan@chandan.git.10@gmail.com>
2021-03-24 16:19:54 +0530

    LSB and MSB check

---
+++ ts_codes/bitwise_cds/msb_check.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+void print_bin(int data)
+{
+	int i;
+  for(i=31;i>=0;i--)
+        {
+                printf("%d",(data>>i)&1);
+        }
+         printf("\n");
+
+
+}
+void main()
+{
+	int data;
+	printf("Enter the data:");
+	scanf("%d",&data);
+	print_bin(data);
+	if(((data>>31)&1)==1)
+		printf("MSB is set\n");
+	else
+		printf("MSB is clear\n");
+
+}
+
