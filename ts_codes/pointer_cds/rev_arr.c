5e1a3a87244b2c881802382295c3aea19bc64696
chandan <chandan@chandan.git.10@gmail.com>
2021-04-06 21:22:58 +0530

    Add: rev array

---
+++ ts_codes/pointer_cds/rev_arr.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#define len 5
+void main()
+{
+	int arr[len]={1,2,3,4,5};
+	int *ptr=arr,i,j,temp;
+	printf("array elements:");
+	for(i=0;i<len;i++)
+	{
+		printf("%d",*ptr+i);
+	}
+	printf("\n");
+	for(i=0,j=len-1;i<j;i++,j--)
+	{
+		temp=*(ptr+i);
+		*(ptr+i)=*(ptr+j);
+		*(ptr+j)=temp;
+	}
+	printf("array after rev:");
+	for(i=0;i<len;i++)
+	{
+		printf("%d",*ptr+i);
+	}
+	printf("\n");
+}
